11a7c2ac08bccc94c217ee1671e112f424faa155
Robin <zwerch1337@gmail.com>
2013-05-14 00:50:04 +0200

    starting project
    beginning gui etc.

---
+++ TStelnet/frmMain.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace TStelnet
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.tbxIP = new System.Windows.Forms.TextBox();
+            this.tbxLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(139, 10);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // tbxIP
+            // 
+            this.tbxIP.Location = new System.Drawing.Point(12, 12);
+            this.tbxIP.Name = "tbxIP";
+            this.tbxIP.Size = new System.Drawing.Size(121, 20);
+            this.tbxIP.TabIndex = 1;
+            this.tbxIP.Text = "91.121.193.155:10011";
+            // 
+            // tbxLog
+            // 
+            this.tbxLog.Location = new System.Drawing.Point(12, 39);
+            this.tbxLog.Multiline = true;
+            this.tbxLog.Name = "tbxLog";
+            this.tbxLog.ReadOnly = true;
+            this.tbxLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbxLog.Size = new System.Drawing.Size(501, 187);
+            this.tbxLog.TabIndex = 3;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 238);
+            this.Controls.Add(this.tbxLog);
+            this.Controls.Add(this.tbxIP);
+            this.Controls.Add(this.btnConnect);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "TStelnet - by r3t4rdun1c0rns";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox tbxIP;
+        private System.Windows.Forms.TextBox tbxLog;
+    }
+}
+
---
+++ TStelnet/frmMain.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+
+namespace TStelnet
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        //TcpClient connector = new TcpClient();
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            string ip;
+            int port;
+            ip = tbxIP.Text.Substring(0,tbxIP.Text.LastIndexOf(":"));
+            port = Convert.ToInt32(tbxIP.Text.Substring(tbxIP.Text.LastIndexOf(":")+1));
+            addtolog("Starting to connect to " + ip + " on port " + port + ".");
+
+            //connector.ConnectAsync(ip, port);
+        }
+
+        #region helpingfunctions
+        private void addtolog(object what)
+        {
+            DateTime currentDate = DateTime.Now;
+            string dtHour, dtMinute, dtSecond;
+            dtHour = withzero(currentDate.Hour);
+            dtMinute = withzero(currentDate.Minute);
+            dtSecond = withzero(currentDate.Second);
+
+            const string newline = "\r\n";
+
+            tbxLog.Text += "[" + dtHour + ":" + dtMinute + ":" + dtSecond + "] ";
+            tbxLog.Text += what.ToString() + newline;
+            tbxLog.SelectionStart = tbxLog.Text.Length;
+            tbxLog.ScrollToCaret();
+        }
+
+        private string withzero(int number)
+        {
+            if (number < 10)
+            {
+                return "0" + Convert.ToString(number);
+            }
+            else
+            {
+                return Convert.ToString(number);
+            }
+        }
+        #endregion
+    }
+}
